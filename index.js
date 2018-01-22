72536534c491de1c4ab0bcc4961c97da6b692dcb
Jay Bednar <ironviralmedia@gmail.com>
2018-01-22 18:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ function checkCollision(rock) {
 
 function createRock(x) {
   const rock = document.createElement('div')
-  x = function getRandomIntInclusive(min, max) {
+  
+  function getRandomIntInclusive(min, max) {
   min = Math.ceil(0);
   max = Math.floor(380);
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
