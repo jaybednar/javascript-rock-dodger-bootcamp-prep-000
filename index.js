d57456f530373250cb4c428099350cf925648428
Jay Bednar <ironviralmedia@gmail.com>
2018-01-22 17:52:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function checkCollision(rock) {
 
 function createRock(x) {
   const rock = document.createElement('div')
-
+  const x = Math.floor(Math.random )
   rock.className = 'rock'
   rock.style.left = `${x}px`
 
